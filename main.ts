e979eaeb1534cbae0f6563b2bdbe0c132b1440f7
Marcelo Fernandes <37639856+tchelomgf@users.noreply.github.com>
2022-01-21 09:02:15 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    MC = MC + 0.007
+})
+input.onButtonPressed(Button.B, function () {
+    MC = MC + 0.140
+})
 serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
     Comm = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
     if (Comm == "ADR 6") {
@@ -9,35 +15,49 @@ serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () 
     } else if (Comm == "PV?") {
         serial.writeLine("" + (PV))
     } else if (Comm == "MV?") {
-        serial.writeLine("" + PV + randint(0, 10))
+        if (OUT == "ON") {
+            serial.writeLine("" + (PV + randint(-0.1, 0.1)))
+        } else {
+            serial.writeLine("0.0000")
+        }
     } else if (Comm.substr(0, 3) == "PV ") {
-        PV = Comm.split(" ")[1]
+        PV = parseFloat(Comm.split(" ")[1])
         serial.writeLine("OK")
     } else if (Comm == "PC?") {
         serial.writeLine("" + (PC))
     } else if (Comm == "MC?") {
-        serial.writeLine("" + PC + randint(0, 10))
+        if (OUT == "ON") {
+            serial.writeLine("" + (MC + randint(-0.002, 0.002)))
+        } else {
+            serial.writeLine("0.0000")
+        }
     } else if (Comm.substr(0, 3) == "PC ") {
-        PC = Comm.split(" ")[1]
+        PC = parseFloat(Comm.split(" ")[1])
         serial.writeLine("OK")
     } else if (Comm == "OUT?") {
         serial.writeLine(OUT)
     } else if (Comm == "OUT 0") {
         OUT = "OFF"
+        MC = 0
+        led.unplot(0, 0)
         serial.writeLine("OK")
     } else if (Comm == "OUT 1") {
         OUT = "ON"
+        led.plot(0, 0)
         serial.writeLine("OK")
     } else {
         serial.writeLine("C01")
+        basic.showString(Comm)
     }
-
-    //basic.showString(Comm)
 })
-let PC = ""
-let PV = ""
 let Comm = ""
 let OUT = ""
+let PV = 0
+let PC = 0
+let MC = 0
+PC = 1
+MC = 0
+PV = 0
 OUT = "OFF"
 serial.redirect(
 SerialPin.USB_TX,
